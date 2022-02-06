ac452f9019f026de38818313954a27942b901563
aldimhernandez <aldana-hernandez@hotmail.com>
2022-02-06 13:25:51 -0300

    create Person Class

--- src/com/admCursos/domain/Person.java
+++ src/com/admCursos/domain/Person.java
@@ -2,9 +2,41 @@ package com.admCursos.domain;
 
 public class Person {
     //Attributes
+    private String firstName;
+    private String lastName;
+    private int studentId;
 
     //Constructor
+    public Person(String firstName, String lastName, int studentId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentId = studentId;
+    }
 
     //Methods
     //Getters and Setters
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
 }
