190854e76096335035ee67b7d372ef16610f3253
aldimhernandez <aldana-hernandez@hotmail.com>
2022-02-06 14:06:42 -0300

    create Course Class and generate doc

--- src/com/admCursos/domain/Course.java
+++ src/com/admCursos/domain/Course.java
@@ -1,8 +1,65 @@
 package com.admCursos.domain;
 
+import java.util.List;
+
 public class Course {
     //Attributes
+    private String courseName;
+    private int requiredHours;
+    private List<Student> students;
+    private List<Float> qualifications;
+    private String modality;
+
     //Constructor
+    public Course(String courseName, int requiredHours, String modality) {
+        this.courseName = courseName;
+        this.requiredHours = requiredHours;
+        this.modality = modality;
+    }
+
     //Methods
+    public void addStudent(Student student) {
+
+    }
+    
     //Getters and setters
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getRequiredHours() {
+        return requiredHours;
+    }
+
+    public void setRequiredHours(int requiredHours) {
+        this.requiredHours = requiredHours;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public List<Float> getQualifications() {
+        return qualifications;
+    }
+
+    public void setQualifications(List<Float> qualifications) {
+        this.qualifications = qualifications;
+    }
+
+    public String getModality() {
+        return modality;
+    }
+
+    public void setModality(String modality) {
+        this.modality = modality;
+    }
 }
