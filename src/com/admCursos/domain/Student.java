fe9256f1fe1399dae99bdf808a9be8b87e8625c0
aldimhernandez <aldana-hernandez@hotmail.com>
2022-02-06 13:59:27 -0300

    create Student Class and generate doc

--- src/com/admCursos/domain/Student.java
+++ src/com/admCursos/domain/Student.java
@@ -1,8 +1,42 @@
 package com.admCursos.domain;
 
-public class Student {
+/**
+ * Student Class
+ * @see com.admCursos.domain.Person
+ * */
+
+import java.util.List;
+
+public class Student extends Person {
     //Attributes
+    private int entryYear;
+    private List<Course> course;
+
     //Constructor
+    public Student(String firstName, String lastName, int studentId, int entryYear) {
+        super(firstName, lastName, studentId);
+        this.entryYear = entryYear;
+    }
+
     //Methods
+    public void addCourse(Course course) {
+
+    }
+
     //Getters and setters
+    public int getEntryYear() {
+        return entryYear;
+    }
+
+    public void setEntryYear(int entryYear) {
+        this.entryYear = entryYear;
+    }
+
+    public List<Course> getCourse() {
+        return course;
+    }
+
+    public void setCourse(List<Course> course) {
+        this.course = course;
+    }
 }
